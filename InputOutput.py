5545fb3d111e4681afb45f0c27023601b8e9926e
forzjing <lovage27@foxmail.com>
2018-02-06 00:25:13 +0800

    ;kmkl;

--- InputOutput.py
+++ InputOutput.py
@@ -1,6 +1,18 @@
 print("hello world")
 print("The quick brown fox","jumps over", "the lazy dog")
 print("100 + 200 =",100+200)
-name = input("input your name please \n")
-print("hello! ",name)
+# name = input("input your name please \n")
+# print("hello! ",name)
 
+print("I\"m learning Python")
+print("\\\n\\")
+
+print(r"I'm learing Python")
+print('''line1 line2 line3''')
+
+print("None=",None)
+
+print(10/3)
+print(9/3)
+print(10//3)#获取整数位结果
+print(10&3) #获取余数
